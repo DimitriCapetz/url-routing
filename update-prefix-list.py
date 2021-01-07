c0c2a041038e6f82ccf3eaef46c1cfbfc0edeaf3
Dimitri Capetz <dimitri.capetz@gmail.com>
2021-01-07 16:01:54 -0600

    Initial Commit for placeholding

---
+++ update-prefix-list.py
@@ -0,0 +1,56 @@
+import json
+import tempfile
+from pathlib import Path
+import urllib.request
+import uuid
+
+# Create Unverified SSL Context for Self-Signed Switch Certs
+import ssl
+ssl._create_default_https_context = _create_unverified_https_context
+
+# helper to call the webservice and parse the response
+def webApiGet(methodName, instanceName, clientRequestId):
+    ws = "https://endpoints.office.com"
+    requestPath = ws + '/' + methodName + '/' + instanceName + '?clientRequestId=' + clientRequestId
+    request = urllib.request.Request(requestPath)
+    with urllib.request.urlopen(request) as response:
+        return json.loads(response.read().decode())
+
+# path where client ID and latest version number will be stored
+datapath = Path(tempfile.gettempdir() + '/endpoints_clientid_latestversion.txt')
+# fetch client ID and version if data exists; otherwise create new file
+if datapath.exists():
+    with open(datapath, 'r') as fin:
+        clientRequestId = fin.readline().strip()
+        latestVersion = fin.readline().strip()
+else:
+    clientRequestId = str(uuid.uuid4())
+    latestVersion = '0000000000'
+    with open(datapath, 'w') as fout:
+        fout.write(clientRequestId + '\n' + latestVersion)
+
+# call version method to check the latest version, and pull new data if version number is different
+version = webApiGet('version', 'Worldwide', clientRequestId)
+
+if version['latest'] > latestVersion:
+    print('New version of Office 365 worldwide commercial service instance endpoints detected')
+    # write the new version number to the data file
+    with open(datapath, 'w') as fout:
+        fout.write(clientRequestId + '\n' + version['latest'])
+    # invoke endpoints method to get the new data
+    endpointSets = webApiGet('endpoints', 'Worldwide', clientRequestId)
+    # filter results for Allow and Optimize endpoints, and transform these into tuples with port and category
+    flatIps = []
+    for endpointSet in endpointSets:
+        if endpointSet['category'] in ('Optimize', 'Allow'):
+            ips = endpointSet['ips'] if 'ips' in endpointSet else []
+            category = endpointSet['category']
+            # IPv4 strings have dots while IPv6 strings have colons
+            ip4s = [ip for ip in ips if '.' in ip]
+            tcpPorts = endpointSet['tcpPorts'] if 'tcpPorts' in endpointSet else ''
+            udpPorts = endpointSet['udpPorts'] if 'udpPorts' in endpointSet else ''
+            flatIps.extend([(category, ip, tcpPorts, udpPorts) for ip in ip4s])
+    print('IPv4 IP Address Ranges')
+    print(','.join(sorted(set([ip for (category, ip, tcpPorts, udpPorts) in flatIps]))))
+else:
+    print('Office 365 worldwide commercial service instance endpoints are up-to-date')
